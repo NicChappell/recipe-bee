67396be8d872a441e200223a688add6c98e228a4
NicChappell <nic.chappell@generalassemb.ly>
2020-03-31 07:47:02 -0600

    placeholder page

--- client/src/actions/utilityActions.js
+++
@@ -1,7 +0,0 @@
-// import actions
-import { SET_ROUTER_HEIGHT } from './types'
-
-// calculate router height
-export const setRouterHeight = height => dispatch => {
-    dispatch({ type: SET_ROUTER_HEIGHT, payload: height})
-}
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://nwc:ZkycaEAjHqQgdUga6Xg8rDXd@cluster0-3hhu4.mongodb.net/recipe-bee?retryWrites=true&w=majority',
-    secretOrKey: "weAllKnowLegendsHaveManyNicknames"
-}
