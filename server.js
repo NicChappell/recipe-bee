3c005dec05897c8994ebfee9de93aabf52354ed4
NicChappell <nic.chappell@generalassemb.ly>
2020-07-21 18:27:08 -0600

    deploy troubleshooting

--- server.js
+++ server.js
@@ -36,12 +36,17 @@ app.use('/api/v1/users', users)
 app.use('/api/v1/uploads', uploads)
 
 // serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
+app.use(express.static('client/build'))
+// app.get('*', (req, res) => {
+//     console.log('asdf')
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+// })
 
 // database config
 const db = process.env.DB_URI
